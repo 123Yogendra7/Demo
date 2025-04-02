9e01e65184b2c1d0e2157310304641fdde5c0c9c
YogendraYadav <yogendrayadav8983@gmail.com>
2025-04-02 21:48:42 +0530

    adding chnage in the reverse Name file

--- src/ReverseName.java
+++ src/ReverseName.java
@@ -5,10 +5,11 @@ import java.util.Locale;
 public class ReverseName {
 
     public static void main(String[] args) {
-        String str = "yogendra yadav";
+      String str = "yogendra yadav";
         char[] chArr = str.toCharArray();
         for (int i = chArr.length - 1; i >= 0; i--) {
             System.out.print(chArr[i]);
+            System.out.print(str);
         }
     }
 }
